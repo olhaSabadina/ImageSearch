2b28bed1705d530723a9d054bd7a317230974bfb
Sabadina Olga <olga.sabadina@icloud.com>
2023-08-23 22:17:49 +0300

    transform crop controller

--- ImagesSearch/Builders/CropBuilder.swift
+++ ImagesSearch/Builders/CropBuilder.swift
@@ -13,13 +13,19 @@ class CropBuilder {
     static func createCropVC(_ image: UIImage) ->  CropViewController {
         
         let vc = CropViewController(croppingStyle: .default, image: image)
-        vc.aspectRatioPreset = .presetSquare
-        vc.aspectRatioLockEnabled = true
-        vc.toolbarPosition = .bottom
+        vc.aspectRatioPreset = .preset16x9
+        vc.cropView.alwaysShowCroppingGrid = true
+        vc.cropView.gridOverlayHidden = false
+        vc.cropView.translucencyAlwaysHidden = true
+        vc.cropView.gridOverlayView.backgroundColor = .white.withAlphaComponent(0.3)
         vc.doneButtonTitle = "Save"
+        vc.toolbarPosition = .bottom
         vc.cancelButtonTitle = "Cancel"
-        vc.doneButtonColor = .systemOrange
-        vc.cancelButtonColor = .systemRed
+        vc.rotateButtonsHidden = true
+        vc.resetButtonHidden = true
+        vc.aspectRatioPickerButtonHidden = true
+        vc.doneButtonColor = .white
+        vc.cancelButtonColor = .black
         return vc
     }
 }
